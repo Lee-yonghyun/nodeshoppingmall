6b2c8417c2c7857783799059328347a9a1006810
이용현 <dydwk231@naver.com>
2020-08-13 11:36:33 +0900

    add order router

--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,27 @@
 const express =require('express');
 const router = express.Router();
 
+
+router.get('/',(req,res)=>{
+    res.json({
+        message:'order data 불러오기'
+    })
+})
+
+router.post('/',(req,res)=>{
+    res.json({
+        message:'order data 생성하기'
+    })
+})
+router.put('/',(req,res)=>{
+    res.json({
+        message:'order data update하기'
+    })
+})
+router.delete('/',(req,res)=>{
+    res.json({
+        message:'order data 삭제하기'
+    })
+})
+
 module.exports= router; //라우터를 모듈화 시켜서 (모듈화 시키는 방법 module.exports)
--- server.js
+++ server.js
@@ -8,19 +8,15 @@ const app =express(); // express 함수를 사용하도록 연결하기 (함수�
 // })
 
 //카테고리화 = 라우터(routes) 서버를 여러가지 갈래로 연결하기
-
+//1) product 라우터 만들어 보기!, 2) order 라우터 만들어 보기
 const productRoutes = require('./routes/products')
-
+const orderRoutes=require('./routes/orders')
 app.use('/products' , productRoutes); // .use(req,res) 이므로 5000/product가 요청들어오면, req는 productRoutes로 보낸다.
-
-
-
-
+app.use('/orders',orderRoutes);
 
 
 
 const PORT = 5000; //재사용이 가능하게 상수화 , 포트를 설정해주기 들어가는 출입경로
-
 app.listen(PORT, console.log('server staeted')); // 포트번호를 지정하기, 로그를 찍어준다. .>>호출, 서버가 만들어짐
 
 
