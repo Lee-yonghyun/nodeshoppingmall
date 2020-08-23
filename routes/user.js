005623f9f3201d6c5f716b6e51652c64c2e80d3c
이용현 <dydwk231@naver.com>
2020-08-23 21:49:26 +0900

    #2 user signup (password 암호화)

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,68 @@
 const express = require('express')
 const router = express.Router()
 const userModel = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 // 회원가입
 router.post('/signup',(req,res)=>{
 
-    const newUser = new userModel({
-        name:req.body.username,
-        email:req.body.useremail,
-        password:req.body.userpassword
-    })
 
-    newUser
-        .save()
-        .then(user =>{
-            // console.log(result)
-            res.json({
-                message:'saved data',
-                userInfo:user
-            })
+    bcrypt.hash(req.body.userpassword, 10 , (err,hash) => {
 
-        })
-        .catch(err=>{
-            res.json({
+        if(err){
+            return res.json({
                 message:err.message
             })
-        })
+        }
+        else{
+            const newUser = new userModel({
+                name:req.body.username,
+                email:req.body.useremail,
+                password:hash
+            })
+
+            newUser
+                .save()
+                .then(user =>{
+                    // console.log(result)
+                    res.json({
+                        message:'saved data',
+                        userInfo:user
+                    })
+
+                })
+                .catch(err=>{
+                    res.json({
+                        message:err.message
+                    })
+                })
+        }
+
+    })
+
+
+
+    // const newUser = new userModel({
+    //     name:req.body.username,
+    //     email:req.body.useremail,
+    //     password:req.body.userpassword
+    // })
+    //
+    // newUser
+    //     .save()
+    //     .then(user =>{
+    //         // console.log(result)
+    //         res.json({
+    //             message:'saved data',
+    //             userInfo:user
+    //         })
+    //
+    //     })
+    //     .catch(err=>{
+    //         res.json({
+    //             message:err.message
+    //         })
+    //     })
 })
 
 
