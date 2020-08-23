8847a4864f3b67766679a4a68c8227a1a208b4d6
이용현 <dydwk231@naver.com>
2020-08-23 19:23:49 +0900

    schema validation

--- models/product.js
+++ models/product.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose')
 
 //2 #schema라 하면 하나의 셀을 의미(name과 price)
 const productSchema = mongoose.Schema({
-    name:String,
-    price:Number
+    name:{
+        type:String,
+        required:true // name 데이터가 없으면 오류가 뜨게!
+    },
+    price:{
+        type:Number,
+        required:true
+    }
 })
 
 //3 >> model의 이름은 product 이고 ,내용은 productschema로 하겠다.
