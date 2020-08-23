eb6f44d37e0515c8a7cfb2f19805c92c6158b7ba
이용현 <dydwk231@naver.com>
2020-08-23 21:27:54 +0900

    add user router & router

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// 회원가입
+router.post('/signUp',(req,res)=>{
+
+})
+
+
+// 로그인
+router.post('/login',(req,res)=>{
+
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const dotEnv = require('dotenv')
 dotEnv.config() //구성하겠다.
 require('./config/database')
 
+const productRoutes = require('./routes/products')
+const orderRoutes=require('./routes/orders')
+const userRoutes = require('./routes/user')
 
-// app.use((req,res)=> {
-//     res.json({
-//         message:'It works!'
-//     }); //DB에서 결국 json파일 형태로 파싱되어 옴.
-// })
 app.use(morgan('dev'));
 // app.use(morgan('common')); //다른 형식
 app.use(bodyParser.json());
@@ -20,10 +18,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 //카테고리화 = 라우터(routes) 서버를 여러가지 갈래로 연결하기
 //1) product 라우터 만들어 보기!, 2) order 라우터 만들어 보기
-const productRoutes = require('./routes/products')
-const orderRoutes=require('./routes/orders')
+
+
 app.use('/products' , productRoutes); // .use(req,res) 이므로 5000/product가 요청들어오면, req는 productRoutes로 보낸다.
 app.use('/orders',orderRoutes);
+app.use('/users',userRoutes)
 
 //데이터 베이스 연결 (데이터베이스 비번과 dbname 설정해주기)
 // const dboptions =  {
