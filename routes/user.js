ea43c60e4ae8ae3ea2423c90c970ac67684deaca
이용현 <dydwk231@naver.com>
2020-08-23 21:39:29 +0900

    #1 user signup

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const userModel = require('../models/user')
 
 // 회원가입
-router.post('/signUp',(req,res)=>{
+router.post('/signup',(req,res)=>{
 
+    const newUser = new userModel({
+        name:req.body.username,
+        email:req.body.useremail,
+        password:req.body.userpassword
+    })
+
+    newUser
+        .save()
+        .then(user =>{
+            // console.log(result)
+            res.json({
+                message:'saved data',
+                userInfo:user
+            })
+
+        })
+        .catch(err=>{
+            res.json({
+                message:err.message
+            })
+        })
 })
 
 
