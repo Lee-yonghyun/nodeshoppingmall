8a762234f3e91abdbb01ea058a47f80c671c6e26
이용현 <dydwk231@naver.com>
2020-09-06 18:14:42 +0900

    add  jwt  check function

---
+++ middleware/check-auth.js
@@ -0,0 +1,20 @@
+//미들웨어의 역할은? 발행된 jwt가 제대로 발행(변환)되었는지 안되었는지를 검증하는 코드
+
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(" ")[1] //토큰을 헤더(html)에 담을 것이다.??
+        const decoded = jwt.verify(token, "key") //토큰을 검증하는 코드(비교한다)
+        req.userData = decoded
+        next()
+    }
+    catch (error) {
+        return res.json({
+            message:'Auth failed',
+            //인증실패
+            error:error
+        })
+
+    }
+}
